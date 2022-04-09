97f055725eab887e24be92d99222b562374cc763
Roopesh Saravanan <roopeshsaravanan.dev@gmail.com>
2022-04-09 13:01:47 +0530

    added requirements.txt

--- room_slot_booking/settings.py
+++ room_slot_booking/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-4f33e%%y*j(vr&g%c8yjl7y23)k+y^-fs2p&f*7odyhj&-i!5w')
+
 SECRET_KEY = 'django-insecure-4f33e%%y*j(vr&g%c8yjl7y23)k+y^-fs2p&f*7odyhj&-i!5w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
